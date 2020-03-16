2b859cf45b1682eb0c6c308e702db3ee4334f4c5
rasheed <rachid.aguo@gmail.com>
2020-03-16 19:27:35 +0100

    Creating method to get repos and calling it when vue instance is created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <div id="app">
-    <SingleRepo repoName="Repository Name" />
+    <SingleRepo 
+      v-for="repo in repos"
+      :key="repo.id"
+      :repoName="repo.name"
+    />
   </div>
 </template>
 
 <script>
 import SingleRepo from "./components/SingleRepo.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     SingleRepo
+  },
+  data() {
+    return {
+      repos: [],
+      pageNumber: 1
+    };
+  },
+  methods: {
+    getRepos(pageNb) {
+      return axios
+        .get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${pageNb}`)
+        .then(result => {
+          this.repos = this.repos.concat(result.data.items);
+          console.log(this.repos);
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
+    }
+  },
+  created(){
+    this.getRepos(this.pageNumber);
   }
 };
 </script>
