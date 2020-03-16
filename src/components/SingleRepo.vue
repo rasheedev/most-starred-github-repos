48c45590286d3f5faed12d751351afb3031d1a60
rasheed <rachid.aguo@gmail.com>
2020-03-16 18:58:30 +0100

    Preparing files

---
+++ src/components/SingleRepo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="repo">
+    <h1> {{ repoName }} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleRepo",
+  props: {
+    repoName: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
